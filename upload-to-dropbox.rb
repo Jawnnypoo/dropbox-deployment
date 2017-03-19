c095b9bc9522fa300bf0c9dca71b2899e5aedbbf
John Carlson <jawnnypoo@gmail.com>
2017-03-19 02:22:09 -0500

    Start to account for folders too

--- upload-to-dropbox.rb
+++ upload-to-dropbox.rb
@@ -1,15 +1,25 @@
+#!/usr/bin/env ruby
+
 require 'dropbox_api'
-require 'openssl'
 require 'yaml'
 require 'logger'
-
-# TODO fix some other way
-# https://github.com/google/google-api-ruby-client/issues/235
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 logger = Logger.new(STDOUT)
 logger.level = Logger::WARN
 
+def uploadFile(dropboxClient, file, dropboxPath)
+  fileName = File.basename(file)
+  content = IO.read(file)
+  dropboxClient.upload dropboxPath + "/" + fileName, content
+end
+
+def uploadDirectory(dropboxClient, directoryPath, dropboxPath)
+  Dir.foreach(directoryPath) do |item|
+  next if item == '.' or item == '..'
+    # do work on real items
+    uploadFile(dropboxClient, item, dropboxPath)
+  end
+end
 # Validation
 config = YAML.load_file("upload-to-dropbox.yml")
 testing = false
@@ -19,11 +29,15 @@ if !config["deploy"]
 end
 artifactPath = config["deploy"]["artifacts_path"]
 dropboxPath = config["deploy"]["dropbox_path"]
-# Just for testing...
+
+# Just for testing, load the local oauth token from the file
 if File.file?("testconfig")
   oauth = IO.read "testconfig"
   ENV["DROPBOX_OAUTH_BEARER"] = oauth
   testing = true
+end
+
+if config["deploy"]["debug"]
   logger.level = Logger::DEBUG
   logger.debug("We are in debug mode")
 end
@@ -33,7 +47,11 @@ dropboxClient = DropboxApi::Client.new
 # Upload all files
 logger.debug("Artifact Path: " + artifactPath)
 logger.debug("Dropbox Path: " + dropboxPath)
-artifactFile = File.open(artifactPath)
-artifactFile.inspect
-content = IO.read(artifactFile)
-dropboxClient.upload dropboxPath + "/" + File.basename(artifactFile), content
+isDirectory = File.directory?(artifactPath)
+logger.debug("Is directory: " + "#{isDirectory}")
+if isDirectory
+  uploadDirectory(dropboxClient, artifactPath, dropboxPath)
+else
+  artifactFile = File.open(artifactPath)
+  uploadFile(dropboxClient, artifactFile, dropboxPath)
+end
