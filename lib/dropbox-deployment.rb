b90b3c155cdf1b5ccc206f267fc4b2d422b5306e
John Carlson <jawnnypoo@gmail.com>
2017-05-12 16:40:11 -0500

    Style updates

--- lib/dropbox-deployment.rb
+++ lib/dropbox-deployment.rb
@@ -2,11 +2,12 @@ require 'dropbox_api'
 require 'yaml'
 require 'logger'
 require 'find'
-require 'dotenv'
 require 'pathname'
+require 'dotenv'
 Dotenv.load
 
 module DropboxDeployment
+  # Does the deployment to dropbox
   class Deployer
 
     def initialize
@@ -14,69 +15,69 @@ module DropboxDeployment
       @@logger.level = Logger::WARN
     end
 
-    def uploadFile(dropboxClient, file, dropboxPath)
-      fileName = File.basename(file)
+    def upload_file(dropbox_client, file, dropbox_path)
+      file_name = File.basename(file)
       content = IO.read(file)
-      @@logger.debug("Uploading " + fileName + " to " + dropboxPath + "/" + fileName)
-      dropboxClient.upload dropboxPath + "/" + fileName, content, :mode => :overwrite
+      @@logger.debug('Uploading ' + file_name + ' to ' + dropbox_path + '/' + file_name)
+      dropbox_client.upload dropbox_path + '/' + file_name, content, mode: :overwrite
     end
 
-    def uploadDirectory(dropboxClient, directoryPath, dropboxPath)
-      Find.find(directoryPath) do |file|
-        
-        if !File.directory?(file)
-          currentFileDir = File.dirname(file)
-          if currentFileDir == directoryPath
-            modifedPath = dropboxPath
-          else 
-             # adjust for if we are a subdirectory within the desired saved build folder
-            modifedPath = dropboxPath + "/" + Pathname.new(currentFileDir).relative_path_from(Pathname.new(directoryPath)).to_s
+    def upload_directory(dropbox_client, directory_path, dropbox_path)
+      Find.find(directory_path) do |file|
+
+        unless File.directory?(file)
+          current_file_dir = File.dirname(file)
+          if current_file_dir == directory_path
+            modified_path = dropbox_path
+          else
+            # adjust for if we are a subdirectory within the desired saved build folder
+            modified_path = dropbox_path + '/' + Pathname.new(current_file_dir).relative_path_from(Pathname.new(directory_path)).to_s
           end
 
-          uploadFile(dropboxClient, file, modifedPath)
+          upload_file(dropbox_client, file, modified_path)
         end
       end
     end
 
     def deploy()
       # Validation
-      if !File.file?("dropbox-deployment.yml")
+      unless File.file?('dropbox-deployment.yml')
         puts "\nNo config file found. You need a file called `dropbox-deployment.yml` with the configuration. See the README for details\n\n"
         exit(1)
       end
-      config = YAML.load_file("dropbox-deployment.yml")
+      config = YAML.load_file('dropbox-deployment.yml')
       testing = false
-      if !config.has_key?("deploy")
-      		puts "\nError in config file! Build file must contain a `deploy` object.\n\n"
-      		exit(1)
+      unless config.has_key?('deploy')
+        puts "\nError in config file! Build file must contain a `deploy` object.\n\n"
+        exit(1)
       end
-      artifactPath = config["deploy"]["artifacts_path"]
-      dropboxPath = config["deploy"]["dropbox_path"]
+      artifact_path = config['deploy']['artifacts_path']
+      dropbox_path = config['deploy']['dropbox_path']
 
-      if ENV["DROPBOX_OAUTH_BEARER"].nil?
+      if ENV['DROPBOX_OAUTH_BEARER'].nil?
         puts "\nYou must have an environment variable of `DROPBOX_OAUTH_BEARER` in order to deploy to Dropbox\n\n"
         exit(1)
       end
 
-      if config["deploy"]["debug"]
+      if config['deploy']['debug']
         @@logger.level = Logger::DEBUG
-        @@logger.debug("We are in debug mode")
+        @@logger.debug('We are in debug mode')
       end
 
-      dropboxClient = DropboxApi::Client.new
+      dropbox_client = DropboxApi::Client.new
 
       # Upload all files
-      @@logger.debug("Artifact Path: " + artifactPath)
-      @@logger.debug("Dropbox Path: " + dropboxPath)
-      isDirectory = File.directory?(artifactPath)
-      @@logger.debug("Is directory: " + "#{isDirectory}")
-      if isDirectory
-        uploadDirectory(dropboxClient, artifactPath, dropboxPath)
+      @@logger.debug('Artifact Path: ' + artifact_path)
+      @@logger.debug('Dropbox Path: ' + dropbox_path)
+      is_directory = File.directory?(artifact_path)
+      @@logger.debug("Is directory: #{is_directory}")
+      if is_directory
+        upload_directory(dropbox_client, artifact_path, dropbox_path)
       else
-        artifactFile = File.open(artifactPath)
-        uploadFile(dropboxClient, artifactFile, dropboxPath)
+        artifact_file = File.open(artifact_path)
+        upload_file(dropbox_client, artifact_file, dropbox_path)
       end
-      @@logger.debug("Uploading complete")
+      @@logger.debug('Uploading complete')
     end
   end
 end
